e23a207eaf308b0f39bcc86fb3bbb080ae0ffea4
eyegio <g.segagni@gmail.com>
2017-06-03 16:45:22 -0400

    added header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './Header.css';
+import '../../assets/images/header-img-demo.png';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        This is a header
+        <div  className="wrapper-text">
+          <h1 className="title">CONDUCTR</h1>
+          <h4 className="sub-title">IBM API connect</h4>
+        </div>
       </div>
     );
   }
